151615ffc28170b654c28e8bde8c565aafbee921
bblsnake <2925141441@qq.com>
2019-10-13 00:49:40 +0800

    vue

---
+++ vue/el-layout/src/components/el-row.vue
@@ -0,0 +1,21 @@
+
+<script>
+export default {
+  render (h) {
+    // return 'render 函数'
+    let obj = h(
+      'div',
+      {},
+      'render 函数'
+    )
+    console.log(obj);
+    return obj;
+  }
+}
+</script>
+
+<style scoped>
+.el-row {
+  width: 100%;
+}
+</style>
