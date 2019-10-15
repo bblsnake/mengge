cbf0b8ee5d9bf60b9897b65f0fb908e67def645c
bblsnake <2925141441@qq.com>
2019-10-15 09:32:45 +0800

    vue

---
+++ vue/el-layout/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <el-row>
+      东西
+    </el-row>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import ElRow from './components/el-row'
+export default {
+  name: 'App',
+  components: {
+    ElRow,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
