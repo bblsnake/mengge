dabf3093155e71d194c96f06e1e16040c73d7d75
bblsnake <2925141441@qq.com>
2019-10-17 11:15:34 +0800

    vue

---
+++ vue/good-hover-zoom-image/src/components/Child.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <MySubChild></MySubChild>
+  </div>
+</template>
+
+<script>
+import MySubChild from './SubChild'
+export default {
+  components: {
+    MySubChild
+  }
+}
+</script>
+
+<style>
+
+</style>
