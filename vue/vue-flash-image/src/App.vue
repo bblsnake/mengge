7e640e22727fbf0e024c03e3016375c1ef0becf1
bblsnake <2925141441@qq.com>
2019-10-22 14:36:48 +0800

    vue

---
+++ vue/vue-flash-image/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Main />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Main from './components/Main'
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Main
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
