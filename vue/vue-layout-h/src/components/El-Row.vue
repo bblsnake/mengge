2608ea2e1699ddb3e058f6f1d44f6bc38b5c4b52
bblsnake <2925141441@qq.com>
2019-10-23 14:28:29 +0800

    vue

---
+++ vue/vue-layout-h/src/components/El-Row.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+//   <template>
+// <div class="el-row">
+//   <slot></slot>
+// </div>
+// </template>
+  props: {
+    tag: {
+      tag: String, default: 'div'
+    },
+    jiange: {
+      type: Number
+    }
+  },
+  render (h) {
+    return h(
+      this.tag,
+      {
+        class: ['el-row']
+      },
+      this.$slots.default
+    )
+  }
+}
+</script>
+
+<style scoped>
+.el-row {
+  width: 100%;
+  display: flex;
+}
+</style>
