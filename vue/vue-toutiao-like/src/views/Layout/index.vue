e673cf61fab61575fc35d7bcf3c017dd699ab5e5
bblsnake <2925141441@qq.com>
2019-10-27 17:19:24 +0800

    vue

---
+++ vue/vue-toutiao-like/src/views/Layout/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <header-bar></header-bar>
+    <router-view></router-view>
+    <footer-bar></footer-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
