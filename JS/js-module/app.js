795eab95167963d7c496b49f709f486990dd025b
bblsnake <2925141441@qq.com>
2019-11-01 11:01:19 +0800

    js

---
+++ JS/js-module/app.js
@@ -0,0 +1,16 @@
+requirejs.config({
+  paths: {
+    jq: './jq',
+    swiper: './swiper'
+  }
+})
+// require(['jq'], (jq) => {
+//   document.getElementById('btn').addEventListener('click', () => {
+//     require(['swiper'], (swiper) => {
+//       console.log(1);
+//     })
+//   })
+// })
+require(['./seaMain.js'], (seaMain) => {
+  
+})
